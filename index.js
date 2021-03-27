16ca06fb8bf5feb07f1966a06095d9f9b607a913
Jimmy Dumalang <jimmydumalang@gmail.com>
2021-03-27 13:35:02 +0700

    sequelize simulators

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
-import { schema } from './app/schema';
+import {schema} from './app/schema';
 
 const app = express();
 const PORT = 8080;
 
-const {Awb} = require('./app/models/sql_models');
+const {sequelize: db, Awb} = require('./app/models/sql_models');
+const s = require('sequelize')
 
 // serving static files
 app.use(express.static('public'));
@@ -15,11 +16,74 @@ app.get('/', (req, res) =>
 );
 
 app.get('/awb', async (req, res) => {
-    const awb1 = await Awb.findAll({lock: true,
-        skipLocked: true});
-    const awb2 = await Awb.findAll();
-    res.send({awb1, awb2})
+    res.send(await Awb.findOne({}));
 });
+
+const testLocking = async () => {
+    let t = await db.transaction({
+        // isolationLevel: s.Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED,
+        // type: s.Transaction.TYPES.EXCLUSIVE
+    })
+
+    // let t2 = await db.transaction({
+        // isolationLevel: s.Transaction.ISOLATION_LEVELS.READ_UNCOMMITTED,
+        // type: s.Transaction.TYPES.EXCLUSIVE
+    // })
+
+
+    try {
+
+        const q1 = await Awb.findOne({
+            lock: t.LOCK.UPDATE,
+            skipLocked: true,
+            transaction: t
+        });
+        // const q1 = await db.query(
+        //     "SELECT `id`, `awb`, `used_at`, `created_at`, `updated_at` FROM `awbs` AS `Awb` LIMIT 1 FOR UPDATE",
+        //     {
+        //         type: s.QueryTypes.SELECT,
+        //         transaction: t
+        //     }
+        // )
+        const awb1 = q1[0];
+        console.log(awb1.id);
+        const q2 = await Awb.findOne({
+            lock: t.LOCK.UPDATE,
+            skipLocked: true,
+            transaction:t
+        });
+        // const q2 = await db.query(
+        //     "SELECT `id`, `awb`, `used_at`, `created_at`, `updated_at` FROM `awbs` AS `Awb` LIMIT 1 FOR UPDATE SKIP LOCKED",
+        //     {
+        //         type: s.QueryTypes.SELECT,
+        //         transaction: t
+        //     }
+        // )
+        const awb2 = q2[0];
+        console.log(awb2.id);
+
+        // awb1.used_at = new Date();
+        // await awb1.save({
+        //     transaction:t
+        // });
+
+        // UPDATE `awbs` SET `used_at`='2021-01-01 00:00:00',`updated_at`= NOW() WHERE `id` = 1234;
+
+        setTimeout(async () => {
+            await t.commit()
+            // const awb1Clone = awb1.get({ plain: true });
+            // const awb2Clone = awb2.get({ plain: true });
+            const awb1Clone = awb1;
+            const awb2Clone = awb2;
+            console.log({awb1Clone, awb2Clone})
+        }, 5000);
+    } catch (e) {
+        console.log(e)
+        await t.rollback()
+    }
+}
+
+// testLocking();
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
